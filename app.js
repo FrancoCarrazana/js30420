0c27bf9a7a92a7db405e86368d6a8891d273fcfd
FrancoCarrazana <97490119+FrancoCarrazana@users.noreply.github.com>
2022-07-04 21:39:11 -0300

    primera entrega del proyecto final

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
-function edadActual() {
-  let dia;
-  let mes;
-  let año = 2022;
-  let añoNacimiento;
-  do {
-    dia = parseFloat(prompt("dia de nacimiento"));
-    mes = parseFloat(prompt("mes de nacimiento"));
-    añoNacimiento = parseFloat(prompt("año de nacimiento"));
-  } while (isNaN(dia) || isNaN(mes) || isNaN(añoNacimiento));
-  {
-    alert(
-      "si tu fecha de nacimiento es " +
-        dia +
-        "/" +
-        mes +
-        "/" +
-        añoNacimiento +
-        " tu edad es de " +
-        (año - añoNacimiento) +
-        " años "
-    );
+class user {
+  constructor(nombre, rol, id, tareas = "sentate y relajate") {
+    this.nombre = nombre;
+    this.rol = rol;
+    this.id = id;
+    this.tareas = tareas;
   }
 }
 
-edadActual();
+const usuario1 = new user("Franco Carrazana", "Empleado", 1, "VALIACION");
+const usuario2 = new user("David Pereyra", "Empleado", 2, "PEDIDOS");
+const usuario3 = new user("Federico Celis", "Empleado", 3, "ATENCION");
+const usuario4 = new user("Cristian Bianco", "jefe", 4);
+const miembros = [usuario1, usuario2, usuario3, usuario4];
+// Funcion principal
+function usuarios() {
+  // soliciata un ID para dar un valor a usuarios
+  do {
+    usuarios = parseInt(prompt("Ingrese id"));
+  } while (isNaN(usuarios));
+  // recibe valor de usarios para filtrar y dar como resultado ID
+  const usuariosLogins = miembros.filter((miembro) => miembro.id == usuarios);
+  // recibe el ID y toma sus valores para devolverlos en una alerta
+  return usuariosLogins.forEach((miembros) => {
+    alert(
+      `Bienvenido ${miembros.nombre} (${miembros.rol}), tus tareas de hoy son: ${miembros.tareas}`
+    );
+  });
+}
+
+usuarios();
