ac594b26b579b20093da589710472d7e5d5eb48f
FrancoCarrazana <97490119+FrancoCarrazana@users.noreply.github.com>
2022-07-27 23:33:28 -0300

    nueva entrega

--- root/app.js
+++ root/app.js
@@ -5,19 +5,8 @@ class User {
   }
 }
 
-let tareas = []
+const usuarios = JSON.parse(localStorage.getItem('storageUsuarios')) ?? []
 
-if (localStorage.getItem('storageUsuarios')){
- tareas = JSON.parse(localStorage.getItem('storageUsuarios'))
-} else {
-  localStorage.setItem('storageUsuarios', JSON.stringify(tareas));
-}
-
-if (localStorage.getItem('storageUsuarios')){
-  tareas = JSON.parse(localStorage.getItem('storageUsuarios'))
- } else {
-   localStorage.setItem('storageUsuarios', JSON.stringify(tareas));
- }
 
 const formTareas = document.getElementById('idForm')
 const botonUsuarios = document.getElementById('botonUsuarios')
@@ -28,33 +17,49 @@ e.preventDefault();
 console.log(e)
 let newData = new FormData (e.target)
 const tarea = new User (newData.get('nombre'), newData.get('texto'))
-tareas.push(tarea)
-localStorage.setItem('storageUsuarios', JSON.stringify(tareas))
+usuarios.push(tarea)
+localStorage.setItem('storageUsuarios', JSON.stringify(usuarios))
 formTareas.reset()
+Toastify({
+  text: "Tarea agregada",
+  duration: 3000,
+  close: true,
+  gravity: "top", // `top` or `bottom`
+  position: "right", // `left`, `center` or `right`
+  stopOnFocus: true, // Prevents dismissing of toast on hover
+  style: {
+    background: "linear-gradient(to top left, #217D81, #D19977)",
+  },
+  onClick: function(){} // Callback after click
+}).showToast();
 })
 
 botonUsuarios.addEventListener('click', () => {
   let storageUsuarios = JSON.parse(localStorage.getItem('storageUsuarios'))
   divUsuarios.innerHTML = ""
-  storageUsuarios.forEach((tarea,indice) =>{
-    divUsuarios.innerHTML += `
-    <div class="card border-dark mb-3" id="tarea${indice}" style="max-width: 20rem;">
-  <div class="card-header">Tarea</div>
-  <div class="card-body">
-    <h4 class="card-title">${tarea.nombre}</h4>
-    <p class="card-text">${tarea.texto}</p>
-    <button class="btn btn-danger">Eliminar Tarea</button>
-  </div>
+  storageUsuarios.forEach((usuarios,indice) =>{
+    Swal.fire({
+      title: 'Usuario',
+      text:'Tareas',
+      html:innerHTML+= ` <div class="card border-primary mb-3 swal-fire-1" id="producto${indice}" style="max-width: 20rem;margin: 4px;">
+      <div class="card-header">Header</div>
+      <div class="card-body">
+      <h4 class="card-title">${usuarios.nombre}</h4>
+      <p class="card-text">${usuarios.texto}</p>
+      <button class="btn btn-danger">Eliminar Tarea</button>
 </div>
-    `
-  })
-  storageUsuarios.forEach((tarea,indice)=> {
-    document.getElementById(`tarea${indice}`).lastElementChild.lastElementChild.addEventListener('click', () => {
-      document.getElementById(`tarea${indice}`).remove();
-      tareas.splice(indice, 1);
-      localStorage.setItem('storageUsuarios', JSON.stringify(tareas))
-      console.log(`${tarea.nombre} eliminada`)
-      console.log(tareas)
+</div>`
     })
   })
+  // storageUsuarios.forEach((usuarios,indice)=> {
+  //   document.getElementById(`tarea${indice}`).lastElementChild.lastElementChild.addEventListener('click', () => {
+  //     document.getElementById(`tarea${indice}`).remove();
+  //     usuarios.splice(indice, 1);
+  //     localStorage.setItem('storageUsuarios', JSON.stringify(usuarios))
+  //     console.log(`${tarea.nombre} eliminada`)
+  //     console.log(usuarios)
+  //   })
+  // })
 })
+
+  
